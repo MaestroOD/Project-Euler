0aa1343f0ca98924c677bf44917f9521c97145e3
MaestroOD <132321532+MaestroOD@users.noreply.github.com>
2023-10-22 11:59:04 -0400

    Update Q5.java
    damnnnn look who figured it out

--- Problem 5 - Smallest Multiple/Q5.java
+++ Problem 5 - Smallest Multiple/Q5.java
@@ -4,39 +4,59 @@
 // i understand what i have to do here - just haven't figured out how to code it yet
 // go through all the numbers, you can throw out any number that divides into any number you are adding (since their condition is automatically met if the new number's condition is met)
 // break down these numbers into their prime factors, and make sure those prime factors are stored in your list (ex. 6 -> 2,3, 9 -> 3,3, 16 -> 2,2,2,2)
-// tried dicking around with sets, but need duplicates
+// tried dicking around with sets, but need duplicates to correctly handle numbers with multiples of the same prime factor
 // tried dicking around with lists, but i am dumb - i think this is a solution - i just need to check to see if the set of a number's prime factors is a subset of our master list, and then add whatever is missing to the master set
+// lists have proved fruitful despite my code looking awful
 
 import java.util.*;
 
 public class Q5 {
 
-    static List<Integer> factors = new ArrayList<Integer>();
+    
     public static void main (String[] args) {
 
         long smallestPositive = 1;
         int maxNumber = 20;
+        List<Integer> factors = new ArrayList<Integer>();
+        List<Integer> primeFactors = new ArrayList<Integer>();
 
         for (int i = 1;i<=maxNumber;i++) {
-            factors.add(i);
+            factors.add(Integer.valueOf(i));
             for (int j = 1; j<i; j++) {
                 if (i%j==0) {
-                    factors.remove(j);
+                    factors.remove(Integer.valueOf(j));
                 }
             }
         }
-       Iterator<Integer> factorsIterator = factors.iterator();
+        Iterator<Integer> factorsIterator = factors.iterator();
         while(factorsIterator.hasNext()) {
-           smallestPositive=smallestPositive*factorsIterator.next();
+            List<Integer> tempList = new ArrayList<Integer>();
+            int temp = factorsIterator.next();
+            tempList=primeFactorsBetter(temp);
+
+            Iterator<Integer> primeIterator = tempList.iterator();
+
+
+            // i originally tried .removeAll(templist) but this removed ALL instances of every element in templist, resulting in incorrect output
+            while (primeIterator.hasNext()) { //extremely jank way to "union" two lists - deleting any duplicates and then adding the whole thing
+                primeFactors.remove(primeIterator.next());
+             } 
+            primeFactors.addAll(tempList);
+         }
+        factorsIterator = primeFactors.iterator();
+        while(factorsIterator.hasNext()) {
+            smallestPositive = smallestPositive*factorsIterator.next();
          }
         System.out.println(smallestPositive);
     }
 
-    private void primeFactors (int number) {
+    // original attempt at generating list of prime factors... "depreciated" as it is does not produce correct output in the cases of repeated prime factors (is it dogshit)
+    private static List<Integer> primeFactors (int number) {
         int target=number;
         int factor=2;
         boolean prime;
-        while (factor*factor<=target) {
+        List<Integer> smallfactors = new ArrayList<Integer>();
+        while (factor<=target) {
             prime=true;
             for (int i=2;i*i<=factor;i++) {
                 if (factor%i==0) {
@@ -45,9 +65,41 @@ public class Q5 {
                 }
             }
             if (target%factor==0 && prime==true) {
-                factors.add(factor);
+                smallfactors.add(factor);
             }
             factor=factor+1;
         }
+        return smallfactors;
+    }
+
+    
+    // returns a list of all prime factors of a given number (16->2,2,2,2, 17->17, 18->2,3,3)
+    private static List<Integer> primeFactorsBetter (int number) {
+        int target=number;
+        int factor=2;
+        boolean prime;
+        List<Integer> smallfactors = new ArrayList<Integer>();
+
+        while (factor<=target) {
+            prime=true;
+
+            for (int i =2;i*i<=factor;i++) {
+                if (factor%i==0) {
+                    prime=false;
+                }
+            }
+
+            while(prime) {
+                if (target%factor==0) {
+                    smallfactors.add(Integer.valueOf(factor));
+                    target = target/factor;
+                }
+                else {
+                    prime = false;
+                }
+            }
+            factor = factor+1;
+        }
+        return smallfactors;
     }
 }
